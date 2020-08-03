7747f72b66258c151b9890faeb2ca2e1c9967cc0
Nicole Budiman <nicole.budiman@gmail.com>
2020-08-04 07:49:04 +0800

    feature: google geocoder

---
+++ src/main/java/com/example/restclient2/services/GeocoderService.java
@@ -0,0 +1,39 @@
+package com.example.restclient2.services;
+
+import com.example.restclient2.entities.Site;
+import com.example.restclient2.json.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class GeocoderService {
+    private static final String BASE = "https://maps.googleapis.com/maps/api/geocode/json";
+    private static final String KEY = ""; // TODO: insert api key
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public GeocoderService(RestTemplateBuilder builder) {
+        restTemplate = builder.build();
+    }
+
+    public Site getLatLng(String... address) {
+        String joinedAddress = String.join(",", address);
+        String encodedAddress = "";
+        encodedAddress = URLEncoder.encode(joinedAddress, StandardCharsets.UTF_8);
+
+        Response response = restTemplate.getForObject(
+                String.format("%s?address=%s&key=%s", BASE, encodedAddress, KEY), Response.class);
+
+        return new Site(response.getFormattedAddress(),
+                response.getLocation().getLat(),
+                response.getLocation().getLng());
+    }
+
+
+}
